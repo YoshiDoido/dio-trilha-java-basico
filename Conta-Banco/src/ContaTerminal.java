baa22288fa27100ee3075bbec06b70f73ebac3f2
Gabriel Yoshino Florencio <gabrielyoshinof@gmail.com>
2025-06-30 19:06:56 -0300

    Conta Bancária criada!

--- Conta-Banco/src/ContaTerminal.java
+++ Conta-Banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+
+        String agencia;
+        int numero;
+        String nomeCliente;
+        float saldo;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Sejam bem-vindos ao Sistema de Cadastro de Conta Bancária.\n");
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        numero = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Por favor, digite a Agência (ex: 197-3): ");
+        agencia = sc.nextLine();
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        nomeCliente = sc.nextLine();
+
+        System.out.print("Por favor, digite o saldo inicial: ");
+        saldo = sc.nextFloat();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s e sua conta é %d.\n", nomeCliente, agencia, numero);
+
+        sc.close();
     }
 }
